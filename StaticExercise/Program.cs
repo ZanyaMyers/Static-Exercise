974d1d728ec00b356f6eec3d4fa362673b441db0
ZanyaMyers <myerszanya@gmail.com>
2023-11-16 09:38:57 -0800

    inital commit

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter a temperature in Fahrenheit");
+            double userInput = double.Parse(Console.ReadLine());
+            Console.WriteLine(TempConverter.FahrenheitToCelsius(userInput));
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Enter a temperature in Celsius");
+            double usersInput = double.Parse(Console.ReadLine());
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(usersInput));
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double ftemperature)
+        {
+            // C = (F-32) * 5 / 9; 
+            return (ftemperature - 32) * 5 / 9; 
+        }
+
+        public static double CelsiusToFahrenheit(double ctemperature)
+        {
+            return (ctemperature * 9) / 5 + 32; 
+        }
+    }
+}
